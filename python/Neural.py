Fix backpropagation

python/Neural.py
@@ -451,7 +451,10 @@ class Neural:
                     dEo_per_dnet = np.where(output_idx == correct_idx, output - 1, output)
                 
                 # Save dE/dnet (delta) for output layer
-                delta[output_layer_idx] = dEo_per_dnet
+                if delta[output_layer_idx] == None:
+                    delta[output_layer_idx] = dEo_per_dnet
+                else:
+                    delta[output_layer_idx] += dEo_per_dnet
 
                 # Get dE/dw for each weight
                 dnet_per_dw = (self.__neuron_output[output_layer_idx - 1]).reshape(-1, 1)
@@ -475,8 +478,11 @@ class Neural:
                     dEh_per_dnet = dEh_per_do * self.__do_per_dnet(self.__sum[j], act)
                     
                     # Save delta (dE/dnet) for current hidden layer
-                    delta[j] = dEh_per_dnet
-                    
+                    if delta[j] == None:
+                        delta[j] = dEh_per_dnet
+                    else:
+                        delta[j] += dEh_per_dnet
+
                     # Calculate dE/dw for each neuron in hidden layer
                     dnet_per_dw = (self.__neuron_output[j-1]).reshape(-1, 1)
                     dEh_per_dw = dnet_per_dw * dEh_per_dnet